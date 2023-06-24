9951e15ddd7b6623d75d650c385d2e312d315791
SenkeSem <sen9morf@gmail.com>
2023-06-24 17:51:39 +0300

    v1.0

---
+++ arr.js
@@ -0,0 +1,27 @@
+function order(words){
+    const findNumber = words.match(/[1-9]/g);
+    const arrayWords = words.split(' ');
+    let result = [];
+
+    if (words.length === 0) return words;
+
+    arrayWords.map((item, i) => {
+        let counter = (i + 1).toString();
+
+        const index = findNumber.indexOf(counter);
+        
+        result.push(arrayWords[index]);
+    });
+
+    return result.join(' ');
+}
+
+// console.log(order("is2 Thi1s T4est 3a"));
+
+// Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+
+// Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+// If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+
+// "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
